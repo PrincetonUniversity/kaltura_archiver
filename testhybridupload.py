3d7d8e7249924339bf2f3f7fcb977880f59e35a5
Mark Ratliff <ratliff@princeton.edu>
2017-12-19 18:36:24 -0500

    Test for on-premises file.  Record result.

--- testhybridupload.py
+++ testhybridupload.py
@@ -1,12 +1,18 @@
-#import urllib
 import os
 import glob
+from datetime import datetime
 from KalturaClient import *
 from KalturaClient.Plugins.Core import *
 
-### Pat to transcoded files
+### Video file to upload
+testvideo_file = "./testvideo.mp4"
 
-flavors_dir = "/path/to/dir/containing/flavor_assets/"
+### Path to transcoded files
+flavors_dir = "/pukaltura/content/kalturau/flavor_assets/"
+
+### HTML file where result will be recorded
+results_file = "./kaltura-hybrid-status.html"
+
 
 partnerId = os.getenv("KALTURA_PARTNERID")
 secret = os.getenv("KALTURA_SECRET")
@@ -18,13 +24,9 @@ ktype = KalturaSessionType.ADMIN
 expiry = 432000 # 432000 = 5 days
 privileges = "disableentitlement"
 
-
-def startsession():
-        """ Use configuration to generate KS
-        """
-        ks = client.session.start(secret, userId, ktype, partnerId, expiry, privileges)
-        client.setKs(ks)
-
+###
+#  Upload and transcode test video
+###
 def testUpload():
 
         startsession()
@@ -40,17 +42,26 @@ def testUpload():
 
         return mediaEntry.id
 
+###
+#  Check to see that flavors were written to on-premises filesystem
+###
 def testTranscode(entryid):
 
-        today = datetime.datetime.today().strftime('%Y%m%d')
-        files = glob.glob(flavors_dir + today + "/1814/" + entryid + "*")
+        today = datetime.today().strftime('%Y%m%d')
+        file_path = flavors_dir + today + "/1814/" + entryid + "*"
+        #print ("File path = %s" % (file_path))
+
+        files = glob.glob(file_path)
 
         if len(files) > 0:
-          return true
+          return True
 
         else:
-          return false
+          return False
 
+###
+#  Delete the test video
+###
 def cleanup(entryid):
         client.media.delete(entryid)
 
@@ -58,9 +69,29 @@ def cleanup(entryid):
 # MAIN code
 ###
 
-entryid = testUpload()
+if __name__ == '__main__':
 
-if ! testTranscode(entryid):
-  print "Test FAILED!!!"
+        ks = client.session.start(secret, userId, ktype, partnerId, expiry, privileges)
+        client.setKs(ks)
 
-#cleanup(entryid)
+        # Upload the test video
+        entryid = testUpload()
+
+        # Record the result
+        if not testTranscode(entryid):
+                resultstring="DOWN\n"
+        else:
+                resultstring = "UP\n"
+
+        # Write the result to an HTML file so that Nagios can check it
+        resultsfile = open(results_file, "w")
+        resultsfile.write("<!DOCTYPE html><html><head><title>Kaltura Hybrid Configuration Status</title></head>\n")
+        resultsfile.write("<body><p>\n")
+        resultsfile.write("Kaltura hybrid configuration status is: " + resultstring)
+        resultsfile.write("</p></body></html>")
+        resultsfile.close()
+
+        # Delete the test video
+        cleanup(entryid)
+
+        client.session.end()
