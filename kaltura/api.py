0bbb5a23bd64325c796443085c752bdbd586fc2c
Monika Mevenkamp <monikam@princeton.edu>
2018-10-24 19:58:03 -0400

    count GetClient calls and restart session after 10000

--- kaltura/api.py
+++ kaltura/api.py
@@ -12,6 +12,7 @@ class KalturaLogger(IKalturaLogger):
     def log(self, msg):
         logger.debug("API: " + msg)
 
+__restart__ = 0
 __client__ = None
 __params__ = {
     'url': "https://www.kaltura.com/",
@@ -24,9 +25,21 @@ __params__ = {
 }
 
 def getClient():
-    global __client__, __params__
-    if (reload):
-        pass;
+    """
+    restart Kaltura session after 10000 calls
+    :return: KalturaClient
+    """
+    global __client__, __params__, __restart__
+    __restart__ -= 1
+    if (__restart__ <= 1):
+        __client__.setKs(None)
+        ks = __client__.session.start(__params__['secret'], __params__['user_id'], __params__['ktype'],
+                                  __params__['partner_id'], __params__['expiry'], __params__['privileges'])
+        __client__.setKs(ks)
+        __restart__ = 10000
+        logger.info("KALTURA {} | {} partnerId: {} | session-key: {} ".
+                    format(__client__.getConfig().serviceUrl, __params__['user_id'], __params__['partner_id'], __client__.getKs()))
+
     return __client__;
 
 
@@ -40,12 +53,9 @@ def startSession(partner_id, user_id, secret):
 
     config = KalturaConfiguration(__params__['partner_id'], logger=KalturaLogger())
     config.serviceUrl = __params__['url']
-    client = KalturaClient(config)
-    logger.info("KALTURA SESSION %s with %s partnerId:%s" % (client.getConfig().serviceUrl, __params__['user_id'], __params__['partner_id']))
-    ks = client.session.start(__params__['secret'], __params__['user_id'], __params__['ktype'],
-                              __params__['partner_id'], __params__['expiry'], __params__['privileges'])
-    client.setKs(ks)
-    __client__ = client
+    __client__  = KalturaClient(config)
+    # trigger session start
+    getClient()
 
     return None
 
