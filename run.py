eecee503d500ba9471ac37d343809269ebc3ddcb
Monika Mevenkamp <monikam@princeton.edu>
2018-11-06 15:16:42 -0500

    run.py: adapt to max page_size 500

--- run.py
+++ run.py
@@ -7,15 +7,25 @@ import os
 
 tsv = "list_creators-{}.tsv"
 log = "list_creators-{}.log"
-rng = range(1,15)
-page_size = 1000
-cmd = 'python kaltura_aws.py list  -n  --first_page {} --page_size {} --max_entries {}'
+rng = range(1,27)
+page_size = 500
+cmd = 'python kaltura_aws.py list --first_page {} --page_size {} --max_entries {}'
 
-def cmd_i(i):
-    return cmd.format(i, page_size, page_size)
+def format_cmd_i(page_i, max):
+    c = cmd.format(page_i, page_size, max)
+    print(c)
+    return c
+
+def cmd_i(page_i, max):
+    cmd = format_cmd_i(page_i, max)
+    ftsv = open(tsv.format(page_i), "w")
+    flog = open(log.format(page_i), "w")
+    p = subprocess.Popen(cmd, stdout=ftsv, stderr=flog, close_fds=True, env=os.environ, cwd=os.getcwd(), shell=True)
+    p.communicate()
 
 if __name__ == '__main__':
-    for i in  rng:
-        ftsv = open(tsv.format(i), "w")
-        flog = open(log.format(i), "w")
-        p = subprocess.Popen(cmd_i(i), stdout=ftsv, stderr=flog, close_fds=True, env=os.environ, cwd=os.getcwd(), shell=True)
+    cmd_i( 1, 5000)
+    cmd_i(11, 5000)
+    cmd_i(21, 5000)
+
+
