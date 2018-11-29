12b0b03494990767cb86cf5401b1b6956da2e73b
Monika Mevenkamp <monikam@princeton.edu>
2018-11-29 13:00:18 -0500

    look at incomingemail enabled and include sponsors for service accounts

--- add_ldap_status.py
+++ add_ldap_status.py
@@ -3,11 +3,31 @@ import sys, argparse
 import envvars
 from puldap import PuLdap, LDAP_ENV_VARS
 
-LDAP_QUERY_PATTERN = '&(uid={})(|(puresource=authentication=enabled)(puresource=authentication=goingaway))'
+LDAP_QUERY_UID_PATTERN = 'uid={}'
+LDAP_QUERY_SPONSOR_PATTERN = 'universityid={}'
 URL_PATTERN =  'https://kmc.kaltura.com/index.php/kmcng/content/entries/entry/{}/metadata'
-def _build_ldap_query_expression(uid):
-    expr =  LDAP_QUERY_PATTERN.format(uid)
+
+def _build_ldap_uid_expr(uid):
+    expr =  LDAP_QUERY_UID_PATTERN.format(uid)
     return expr
+
+def _build_ldap_sponsor_expr(sponsorid):
+    expr =  LDAP_QUERY_SPONSOR_PATTERN.format(sponsorid)
+    return expr
+
+def _ldap_empty_results(fields):
+    results = {}
+    for f in fields:
+        results[f] = ''
+    return results
+
+def _ldap_get_match(ldap, expr, fields):
+    res_type, res_data, res_msgid, res_controls = next(ldap.all_results_with(expr, fields))
+    results = _ldap_empty_results(fields)
+    data = res_data[0][1]
+    for k in data.keys():
+        results[k] = data[k][0]
+    return results
 
 
 class AddLdapStatusParser(envvars.ArgumentParser):
@@ -39,34 +59,38 @@ def _enhance(tsvin, entryid_col, netid_col, ldap):
     entry_idx = header.index(entryid_col)
     netid_idx = header.index(netid_col)
 
-    print("\t".join([line, 'status', 'name', 'org_unit']))
+    print("\t".join([line, 'status', 'puincomingemail', 'pustatus', 'sponsor', 'name', 'org_unit']))
     for line in tsvin.readlines():
         line = line.strip()
         cols = line.split("\t")
 
         # get netid and chop off '@....' if given as email
         netid = cols[netid_idx]
+        eid = cols[entry_idx]
         if (netid.find('@') >= 0 ):
             netid = netid[:netid.find('@')]
-        #determine ldap status, name, and org-unit
-        name, active, ou  = '', 'inactive', ''
-        try:
-            res_type, res_data, res_msgid, res_controls = next(ldap.all_results_with(_build_ldap_query_expression(netid), ['displayName', 'cn', 'ou']))
-            data = res_data[0][1]
-            if data['displayName']:
-                name  = data['displayName'][0]
-            elif data['cn']:
-                name = data['cn'][0]
-            if ('ou' in data): 
-                ou = data['ou'][0]
-            else: 
-                ou = ''
-            status = 'active'
-        except StopIteration as e:
-            status = 'inactive'
-            name = ''
+        _do_entry(ldap, eid, netid)
 
-        print("\t".join([line, status, name, ou]))
+def _do_entry(ldap, eid, netid):
+        #determine ldap status, name, and org-unit
+        fields = ['universityidref', 'displayName', 'cn', 'ou', 'pustatus', 'puincomingemail']
+        try:
+            results = _ldap_get_match(ldap, _build_ldap_uid_expr(netid), fields)
+            if not results['displayName']:
+                results['displayName'] = results['cn'][0]
+            if results['universityidref']:
+                sponsor = _ldap_get_match(ldap, _build_ldap_sponsor_expr(results['universityidref']), ['uid'])['uid']
+                results['sponsor'] = sponsor
+                _do_entry(ldap, eid, sponsor)
+            results['status'] = 'active'
+
+        except StopIteration as e:
+            results = _ldap_empty_results(fields)
+            results['status'] = 'inactive'
+            pass;
+        if (not results.has_key('sponsor')):
+            results['sponsor'] = ''
+        print("\t".join([eid, netid, results['status'], results['puincomingemail'], results['pustatus'], results['sponsor'], results['displayName'], results['ou']]))
 
 if __name__ == '__main__':
     parser = AddLdapStatusParser.create()
