11a85ff2da866c5334d71d351c8aa82f7dc7649c
Monika Mevenkamp <monikam@princeton.edu>
2019-11-20 13:27:33 -0500

    test_kaltura_aws: better names for for hard coded entry ids

--- src/test/test_kaltura_aws.py
+++ src/test/test_kaltura_aws.py
@@ -15,16 +15,16 @@ class TestKalturaAwsCli(TestKaltura):
     @classmethod
     def setUpClass(cls):
         print("-- setUpClass {}".format(cls))
-        TestKalturaAwsCli.entry_id = TestKaltura.TEST_ID_NO_S3_COPY
+        TestKalturaAwsCli.no_s3_copy_id = TestKaltura.TEST_ID_NO_S3_COPY
         TestKalturaAwsCli.no_orig_id = TestKaltura.TEST_ID_NO_ORIGINAL
-        entry = kaltura.api.getClient().media.get(TestKalturaAwsCli.entry_id)
+        entry = kaltura.api.getClient().media.get(TestKalturaAwsCli.no_s3_copy_id)
         if (kaltura.MediaEntry(entry).getOriginalFlavor().getSize() < 350):
             print("ABORT: based on size ORIGINAL FLAVOR seems to be replacement video")
             sys.exit(1)
         if entry.getTags():
-            print("ABORT: entry {} has tags - probably left over from previous run failures".format(TestKalturaAwsCli.entry_id))
+            print("ABORT: entry {} has tags - probably left over from previous run failures".format(TestKalturaAwsCli.no_s3_copy_id))
             sys.exit(1)
-        assert(not aws.s3_exists(TestKalturaAwsCli.entry_id, TestKaltura.bucket))
+        assert(not aws.s3_exists(TestKalturaAwsCli.no_s3_copy_id, TestKaltura.bucket))
 
     @classmethod
     def tearDownClass(cls):
@@ -42,13 +42,13 @@ class TestKalturaAwsCli(TestKaltura):
 
     def setUp(self):
         super(TestKalturaAwsCli, self).setUp()
-        self.entry_id = TestKaltura.TEST_ID_NO_S3_COPY
-        self.mentry = kaltura.MediaEntry(kaltura.api.getClient().media.get(self.entry_id))
-        self.mentry.reload()
-        self.entry =  self.mentry.entry
+        self.entry_id_not_in_s3 = TestKaltura.TEST_ID_NO_S3_COPY
+        self.mentry_not_in_s3 = kaltura.MediaEntry(kaltura.api.getClient().media.get(self.entry_id_not_in_s3))
+        self.mentry_not_in_s3.reload()
+        self.entry_not_in_s3 =  self.mentry_not_in_s3.entry
 
     def testa_entry_is_healthy(self):
-            argv = ['health', '-i', self.entry_id]
+            argv = ['health', '-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertEqual(rc, 0)
 
@@ -88,63 +88,63 @@ class TestKalturaAwsCli(TestKaltura):
         self.assertEqual(rc, 0)
 
     def testab_download(self):
-        argv = ['download', '-i', self.entry_id]
+        argv = ['download', '-i', self.entry_id_not_in_s3]
         rc = kaltura_aws._main(argv)
         self.assertEqual(rc, 0)
         # check not empty
-        self.assertGreater(os.stat(self.entry_id + ".mp4").st_size, 0)
-        os.remove(self.entry_id + ".mp4")
+        self.assertGreater(os.stat(self.entry_id_not_in_s3 + ".mp4").st_size, 0)
+        os.remove(self.entry_id_not_in_s3 + ".mp4")
 
     def testb_replace_video_without_s3copy(self):
-            argv = ['replace_video', '-i', self.entry_id]
+            argv = ['replace_video', '-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertNotEqual(rc, 0)
 
     def testb_add_archive_tag_only_is_unhealthy(self):
-            self.mentry.addTag(kaltura_aws.SAVED_TO_S3, doUpdate=True)
-            self.mentry.reload()
-            argv = ['health', '-i', self.entry_id]
+            self.mentry_not_in_s3.addTag(kaltura_aws.SAVED_TO_S3, doUpdate=True)
+            self.mentry_not_in_s3.reload()
+            argv = ['health', '-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertNotEqual(rc, 0)
-            self.mentry.delTag(kaltura_aws.SAVED_TO_S3, doUpdate=True)
-            self.mentry.reload()
+            self.mentry_not_in_s3.delTag(kaltura_aws.SAVED_TO_S3, doUpdate=True)
+            self.mentry_not_in_s3.reload()
 
     def testcp_s3copy_dryrun(self):
             #DRYRUN
-            argv = ['s3copy','-i', self.entry_id]
+            argv = ['s3copy','-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertEqual(rc, 0)
             # no TAGS not in s3
-            kaltura.MediaEntry(self.entry).reload()
-            self.assertFalse(self.entry.getTags())
-            self.assertFalse(aws.s3_exists(self.entry_id, self.bucket))
+            kaltura.MediaEntry(self.entry_not_in_s3).reload()
+            self.assertFalse(self.entry_not_in_s3.getTags())
+            self.assertFalse(aws.s3_exists(self.entry_id_not_in_s3, self.bucket))
 
     def testcp_s3copy_run(self):
             for _ in range(0,2):
-                argv = ['s3copy', '--s3copy',  '-i', self.entry_id]
+                argv = ['s3copy', '--s3copy',  '-i', self.entry_id_not_in_s3]
                 rc = kaltura_aws._main(argv)
                 self.assertEqual(rc, 0)
                 # no TAGS not in s3
-                kaltura.MediaEntry(self.entry).reload()
-                self.assertTrue(kaltura_aws.SAVED_TO_S3,  self.entry.getTags())
-                self.assertTrue(aws.s3_exists(self.entry_id, self.bucket))
+                kaltura.MediaEntry(self.entry_not_in_s3).reload()
+                self.assertTrue(kaltura_aws.SAVED_TO_S3, self.entry_not_in_s3.getTags())
+                self.assertTrue(aws.s3_exists(self.entry_id_not_in_s3, self.bucket))
 
     def testcp_add_place_holder_only_is_unhealthy(self):
-            self.mentry.addTag(kaltura_aws.PLACE_HOLDER_VIDEO, doUpdate=True)
-            self.mentry.reload()
-            argv = ['health', '-i', self.entry_id]
+            self.mentry_not_in_s3.addTag(kaltura_aws.PLACE_HOLDER_VIDEO, doUpdate=True)
+            self.mentry_not_in_s3.reload()
+            argv = ['health', '-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertNotEqual(rc, 0)
-            self.mentry.delTag(kaltura_aws.PLACE_HOLDER_VIDEO, doUpdate=True)
-            self.mentry.reload()
+            self.mentry_not_in_s3.delTag(kaltura_aws.PLACE_HOLDER_VIDEO, doUpdate=True)
+            self.mentry_not_in_s3.reload()
 
     def testr_replace_video_with_s3copy_dryrun(self):
-            argv = ['replace_video', '-i', self.entry_id]
+            argv = ['replace_video', '-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertEqual(rc, 0)
 
     def testr_replace_video_run(self):
-        argv = ['replace_video', '--replace', '-i', self.entry_id]
+        argv = ['replace_video', '--replace', '-i', self.entry_id_not_in_s3]
         for _ in range(0,2):
             rc = kaltura_aws._main(argv)
             self.assertEqual(rc, 0)
@@ -152,12 +152,12 @@ class TestKalturaAwsCli(TestKaltura):
 
 
     def testre_restore_video_from_s3_dryrun(self):
-            argv = ['restore_from_s3', '-i', self.entry_id]
+            argv = ['restore_from_s3', '-i', self.entry_id_not_in_s3]
             rc = kaltura_aws._main(argv)
             self.assertEqual(rc, 0)
 
     def testre_restore_video_from_s3_run(self):
-            argv = ['restore_from_s3', '--restore', '-i', self.entry_id]
+            argv = ['restore_from_s3', '--restore', '-i', self.entry_id_not_in_s3]
             for _ in range(0,2):
                 rc = kaltura_aws._main(argv)
                 self.assertEqual(rc, 0)
@@ -166,8 +166,8 @@ class TestKalturaAwsCli(TestKaltura):
     def _wait_original_ready(self):
         while (True):
             kaltura_aws._pause(5, True)
-            self.mentry.reload()
-            if (kaltura.Flavor(self.mentry.getOriginalFlavor()).isReady()):
+            self.mentry_not_in_s3.reload()
+            if (kaltura.Flavor(self.mentry_not_in_s3.getOriginalFlavor()).isReady()):
                 break;
 
 if __name__ == '__main__':
